cb179a368025ef50c615d48d3e3c229486eae5cd
Queite Schneider Castiglioni <queitesc@gmail.com>
2022-09-23 08:38:35 -0300

    :construction: feat: adiciona imagens via rota estática

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+app.use(express.static('public'));
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use('/login', authRouter);
--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Card.css';
 
 function Card({ price, img, title, qtd, index }) {
   return (
