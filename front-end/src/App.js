33bd606e9c881cc64e2de4827c05769ce52c551d
Joao Antonio <joaopsilva.dev@gmail.com>
2022-09-26 15:36:00 -0300

    feat: :construction: iniciando implementação de front end da tela de pedidos e card dos pedidos

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import Register from './pages/Register';
+import Orders from './pages/Orders';
 import AppProvider from './context/appProvider';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/login" element={ <Login /> } />
           <Route path="/customer/products" element={ <Products /> } />
           <Route path="/register" element={ <Register /> } />
+          <Route path="/customer/orders" element={ <Orders /> } />
         </Routes>
       </Router>
     </AppProvider>
